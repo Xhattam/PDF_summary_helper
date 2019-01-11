f423dd456d4f7213e989bdb493f0db1b0ff04bbf
Xhattam <jessica.tanon@gmail.com>
2019-01-11 16:03:40 +0000

    added one level of depth from main file (if main file contains reference to another tex source, the content of this other source will be added to the output, and parsed normally). If these reference files contain other references to other inputs, the input call is simply added. Might be good to add later ?

--- main/src/parse_latex.py
+++ main/src/parse_latex.py
@@ -28,8 +28,8 @@ def get_sections(text):
     :param text: content of the latex file
     :return: iterator with section/subsection names, in latex format: `\section{section name}`
     """
-    tmp = re.finditer(r"^\\(s(?:ubs){0,2}ection|paragraph){.*?}", text, re.MULTILINE)
-    return [(e.start(), e.group()) for e in tmp]
+    tmp = re.finditer(r"^\\(s(?:ubs){0,2}ection|paragraph|input){.*?}", text, re.MULTILINE)
+    return [(e.start(), e.end(), e.group()) for e in tmp]
 
 
 def get_figure(text):
@@ -43,22 +43,39 @@ def get_figure(text):
                        '{}'.format(patt) +
                        r')\*{0,1})}(?:.*?)\\end{(?P=sec)})', text, re.S)
 
-    return [(e.start(), e.group().strip()) for e in tmp]
+    return [(e.start(), e.end(), e.group().strip()) for e in tmp]
 
 
 def remove_comments(text):
-    """ Removes comments
+    """ Removes inline and general comments (indicated with %), while keeping literal %
+    Also removes training whitespaces
     :param text: latex file content
     :return: content without comments
     """
-    clean = "\n".join([l for l in text.split("\n") if not l.startswith("%")])
-    return re.sub(r'\n{3,}', '\n', clean)
+    no_comments = []
+    lines = [l.rstrip(" ") for l in text.split("\n")]
+    print(lines[10:20])
+    for line in lines:
+        if not "%" in line:
+            no_comments.append(line)
+        else:
+            if not line.startswith("%"):
+                if line == '':
+                    no_comments.append(line)
+                else:
+                    line = line.replace('\%', 'TMP_PERCENT')
+                    line = re.sub(" *%.*", '', line)
+                    line = line.replace('TMP_PERCENT', r'\%')
+                    if line != "":
+                        no_comments.append(line)
+
+    return "\n".join(no_comments)
 
 
 def get_appendix(text):
     """ Extracts the appendix header (no content) """
     tmp = re.finditer(r'(^\\appendix$)', text, re.M)
-    return [(e.start(), e.group()) for e in tmp]
+    return [(e.start(), e.end(), e.group()) for e in tmp]
 
 
 def get_all(text):
@@ -71,16 +88,52 @@ def get_all(text):
     text = remove_comments(text)
     sections = re.split(r'(\\section{.*})', text)
     header = sections[0].strip()
-    result = [(0, header)]
+    result = [(0, 1, header)]
     result += get_figure(text)
     result += get_sections(text)
     result += get_references(text)
     result += get_appendix(text)
-    result.sort()
+    result.sort(key=lambda x: x[0])
+    # for e in result:
+    #     print("wut")
+    #     print(e)
+    result = remove_overlappings(result)
 
-    return "\n\n".join(e[1] for e in result).strip() + "\n\n\end{document}\n"
+    return "\n\n".join(e[2] for e in result).strip() + "\n\n\end{document}\n"
 
 
+def rm_ovl(results):
+    res = []
+    i = 0
+    for e in results:
+        curr = e[i]
+        rest = e[i+1:]
+
+
+def remove_overlappings(results):
+    i = 0
+    for e in results:
+        print(e)
+    for e in results:
+        curr = e
+        print("Current: ", i, curr, "\n")
+        j = i + 1
+        while j < len(results):
+            if not is_contained(results[j], e):
+                print("\tNOT CONTAINED: ", j, results[j])
+                i += 1
+                break
+            else:
+                print("\tCONTAINED ! : ",j,  results[j])
+                del results[i+1]
+
+
+        print()
+    return results
+
+
+def is_contained(e1, e2):
+    return e1[0] < e2[1] and e1[1] < e2[1]
 def get_references(content):
     """ Extracts bibliography.
 
@@ -88,7 +141,7 @@ def get_references(content):
     :return: iterator containing each line matching \bilbio.*{.*}
     """
     tmp = re.finditer(r'\\biblio.*{.*}', content)
-    return [(e.start(), e.group()) for e in tmp]
+    return [(e.start(), e.end(), e.group()) for e in tmp]
 
 
 def parser_main(dl_path, ret=False):
@@ -98,19 +151,23 @@ def parser_main(dl_path, ret=False):
     param ret: if true, returns extracted content (for tests)
     """
     files_list = [e for e in os.listdir(dl_path) if e.endswith(".tex")]
+
     latex_content, tex_src_name = None, None
     multiple_inputs = {}
 
     for name in files_list:
-        content = open(os.path.join(dl_path, name), 'r').read()
+        content_fh = open(os.path.join(dl_path, name), 'r')
+        content = content_fh.read()
         if "\\begin{document}" in content:
-            latex_content = content
+            latex_content = remove_comments(content)
             tex_src_name = name
         else:
             name_no_ext = name.rsplit(".", 1)[0]
             multiple_inputs[name_no_ext] = content
+        content_fh.close()
 
     latex_content = concatenate_multiple_sources_into_one(multiple_inputs, latex_content)
+
     if latex_content:
         global TEX_FILE_PATH
         TEX_FILE_PATH = os.path.join(dl_path, tex_src_name)
@@ -128,9 +185,6 @@ def concatenate_multiple_sources_into_one(multiple_inputs, main_tex):
     def replace_input_ref_by_content(m):
         return multiple_inputs[m.groups()[0]]
 
-    print("MULTIPLE CONTENTS")
-    for e in multiple_inputs:
-        print(e)
     return re.sub(r'\\input{(.*?)}', replace_input_ref_by_content, main_tex)
 
 if __name__ == "__main__":
--- test/src/recursive_latex_files_finder_tester.py
+++ test/src/recursive_latex_files_finder_tester.py
@@ -10,17 +10,16 @@ from shutil import rmtree
 from arxiv_downloader_helper import get_download_folder
 import parse_latex as pl
 import logging
-from os import listdir
+import os
 import re
 
 logging.basicConfig(level="INFO")
 
-EXP1 = open("../resources/multiple_inputs_ex1_expected.tex").read()
+class TestMultipleLatexIputs(unittest.TestCase):
 
+    def test_files_search(self):
+        p = "../resources/1901_recursion"
+        a = [os.path.join(dp, f) for dp, dn, fn in os.walk(os.path.expanduser(p)) for f in fn if f.endswith(".tex")]
+        for e in a:
+            print(e)
 
-class TestMultipleInputs(unittest.TestCase):
-
-    def test_this(self):
-        self.maxDiff = None
-        replaced = pl.parser_main("../resources/1901", ret=True)
-        self.assertEqual(EXP1, replaced)
